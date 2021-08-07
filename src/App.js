7cf5c014242f6f3ba18fa970c7b0d76ab5de5ca7
Burak Özkan <burak.ozkan@path.com.tr>
2021-08-07 17:59:28 +0300

    bird functions ready

--- src/App.js
+++ src/App.js
@@ -3,22 +3,52 @@ import './App.css';
 
 const HEIGHT = 500;
 const WIDTH = 800;
-const PIPE_WIDTH = 80;
+const PIPE_WIDTH = 60;
 const MIN_PIPE_HEIGHT = 40;
 const FPS = 120;
 
+class Bird {
+
+  constructor(ctx) {
+    this.ctx = ctx;
+    this.x = 100;
+    this.y = 150;
+    this.gravity = 0;
+    this.velocity = 0.1;
+  };
+
+  draw() {
+    this.ctx.fillStyle = 'red';
+    this.ctx.beginPath();
+    this.ctx.arc(this.x, this.y, 10, 0, Math.PI * 2, true);
+    this.ctx.fill();
+  };
+
+  update = () => {
+    this.gravity += this.velocity;
+    this.gravity = Math.min(4, this.gravity);
+    this.y += this.gravity;
+  };
+
+  jump = () => {
+    this.gravity = -4;
+  }
+
+}
+
 class Pipe {
-  constructor(ctx,height, space) {
-    this.ctx = ctx
+  constructor(ctx, height, space) {
+    this.ctx = ctx;
+    this.isDead = false;
     this.x = WIDTH;
     this.y = height ? HEIGHT - height : 0;
     this.width = PIPE_WIDTH;
     this.height = height || MIN_PIPE_HEIGHT + Math.random() * (HEIGHT - space - MIN_PIPE_HEIGHT * 2);
   };
 
-  draw(){
+  draw() {
     const space = 80;
-    
+
     this.ctx.fillStyle = '#000';
     this.ctx.fillRect(this.x, this.y, this.width, this.height);
     //this.ctx.fillRect(WIDTH, firstPipeHeight + space, PIPE_WIDTH, secondPipeHeight);
@@ -26,6 +56,10 @@ class Pipe {
 
   update = () => {
     this.x -= 1;
+
+    if ((this.x + PIPE_WIDTH) < 0) {
+      this.isDead = true
+    }
   }
 }
 
@@ -35,16 +69,30 @@ class App extends Component {
     super(props);
     this.canvasRef = React.createRef();
     this.frameCount = 0;
-    this.space = 80;
+    this.space = 120;
+    this.pipes = [];
+    this.birds = [];
   }
 
   componentDidMount() {
+    document.addEventListener('keydown', this.onKeyDown);
+    const ctx = this.getCtx();
     this.pipes = this.generatePipes();
-    setInterval(this.gameLoop, 1000 / FPS);
+    this.birds = [new Bird(ctx)];
+    this.lootTest = 'TEST'
+    this.loop = setInterval(this.gameLoop, 1000 / FPS);
   };
 
+  onKeyDown = (e) => {
+    if (e.code === 'Space') {
+      this.birds[0].jump()
+    }
+  }
+
+  getCtx = () => this.canvasRef.current.getContext('2d');
+
   generatePipes = () => {
-    const ctx = this.canvasRef.current.getContext('2d');
+    const ctx = this.getCtx()
     const firstPipe = new Pipe(ctx, null, 80);
     const secondPipeHeight = HEIGHT - firstPipe.height - this.space;
     const secondPipe = new Pipe(ctx, secondPipeHeight, 80);
@@ -57,20 +105,45 @@ class App extends Component {
     this.draw()
   }
 
-  update =() => {
+  update = () => {
     this.frameCount = this.frameCount + 1;
-    if(this.frameCount % 240 === 0){
+    if (this.frameCount % 320 === 0) {
       const pipes = this.generatePipes();
       this.pipes.push(...pipes)
     }
 
+    // update pipe positions
     this.pipes.map(pipe => pipe.update());
+    this.pipes = this.pipes.filter(pipe => !pipe.isDead)
+
+    // update bird positions
+    this.birds.map(bird => bird.update());
+
+    if(this.isGameOver()){
+      alert('game over')
+
+      clearInterval(this.loop);
+    }
   }
 
+  isGameOver = () => {
+    let gameOver = false;
+    this.birds.forEach((bird) => {
+      this.pipes.forEach((pipe) => {      
+      if (bird.y < 0 || bird.y > HEIGHT ||( bird.x > pipe.x && bird.x < pipe.x + pipe.width
+          && bird.y > pipe.y && bird.y < pipe.y + pipe.height)) {
+          gameOver = true
+      };
+    });
+    });
+    return gameOver
+  };
+
   draw = () => {
-    const ctx = this.canvasRef.current.getContext('2d');
-    ctx.clearRect(0,0, WIDTH, HEIGHT)
-    this.pipes.map(pipe => pipe.draw())
+    const ctx = this.getCtx();
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+    this.pipes.map(pipe => pipe.draw());
+    this.birds.map(bird => bird.draw())
   }
 
   render() {
